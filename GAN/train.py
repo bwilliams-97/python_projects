a104bae737c95b7d0afa3fe49f6a9b2117c0cbcf
Ben Williams <t-bewill@microsoft.com>
2020-04-03 17:47:16 +0100

    Tidied up train documentation.

--- GAN/train.py
+++ GAN/train.py
@@ -7,59 +7,86 @@ from tqdm import tqdm
 def train(
     generator: torch.nn.Module, 
     discriminator: torch.nn.Module,
-    n_epochs: int,
-    fixed_noise: torch.tensor,
     train_loader: torch.utils.data.DataLoader,
-    latent_size: int
-    ):
+    **kwargs
+    ) -> None:
+    """
+    Training a GAN has two key phases. For each epoch and minibatch:
+        1. Run real images (label=1) and fake images from generator network (label=0) through discriminator.
+        Optimise discriminator to predict fake and real images.
+        2. Run fake images from generator through discriminator.
+        Optimise generator to fool discriminator network.
+
+        Optimisation for each network is done separately.
+    
+    Params:
+        generator (torch.nn.Module): Generator network, generates image from input latent vector.
+        discriminator (torch.nn.Module): Discriminator network, predicts likelihood of image being
+            real.
+        train_loader (torch.utils.data.DataLoader): Input PyTorch DataLoader object.
+
+    Keyword args:
+        learning_rate (float): Learning rate of both optimisers.
+        n_epochs (int): Number of epochs to train for.
+        latent_size (int): Size of latent vector generator takes as input.
+        fixed_noise (torch.tensor): Fixed noise vector that is used to generate saved images
+            for generator assessment.
+    """
 
     criterion = nn.BCELoss()
 
-    discriminator_optimiser = optim.Adam(discriminator.parameters(), lr=0.0002)
-    generator_optimiser = optim.Adam(generator.parameters(), lr=0.0002)
+    learning_rate = kwargs["learning_rate"]
+    discriminator_optimiser = optim.Adam(discriminator.parameters(), lr=learning_rate)
+    generator_optimiser = optim.Adam(generator.parameters(), lr=learning_rate)
 
-    for epoch in range(n_epochs):
+    for epoch in tqdm(range(kwargs["n_epochs"])):
         for i, data in enumerate(train_loader, 0):
-            print(i)
-            ###################################
-            # Update D
-            ###################################
             discriminator.zero_grad()
 
-            real_data = data[0]
-            batch_size = real_data.size(0)
+            # First run discriminator on real images
+            real_imgs = data[0]
+            batch_size = real_imgs.size(0)
             label = torch.ones((batch_size, ))
 
-            output = discriminator(real_data)
-            error_real = criterion(output, label)
+            real_discriminator_predictions = discriminator(real_imgs)
+            # How well does discriminator recognise real images?
+            error_real = criterion(real_discriminator_predictions, label)
             error_real.backward()
 
-            # train with fake
-            latent_noise = torch.randn(batch_size, latent_size, 1, 1)
-            fake = generator(latent_noise)
-            output = discriminator(fake.detach())
-            error_fake = criterion(output, label)
+            # Second run discriminator on fake generated images
+            latent_noise = torch.randn(batch_size, kwargs["latent_size"], 1, 1)
+            fake_imgs = generator(latent_noise)
+
+            # Overwrite label, as images are now fake.
+            label.fill_(0.)
+            fake_descriminator_predictions = discriminator(fake_imgs.detach())
+            # How well does discriminator recognise fake images?
+            error_fake = criterion(fake_descriminator_predictions, label)
             error_fake.backward()
 
+            # Update discriminator to improve at predicting which images are real and fake
             total_error = error_real + error_fake
             discriminator_optimiser.step()
 
-            ###################################
-            # Update G
-            ###################################
             generator.zero_grad()
 
-            output = discriminator(fake)
-            error_gen = criterion(output, label)
+            # Now run discriminator on fake images again
+            fake_descriminator_predictions = discriminator(fake_imgs)
+
+            # Overwrite label, as we want discriminator to falsely classify generated images
+            label.fill_(1.)
+            # How well does generator fool discriminator?
+            error_gen = criterion(fake_descriminator_predictions, label)
             error_gen.backward()
 
+            # Update generator to improve at fooling discriminator
             generator_optimiser.step()
-
+            print(i)
             if i % 100 == 0:
-                save_image(real_data,
+                save_image(real_imgs,
                         '%s/real_samples.png' % "images",
                         normalize=True)
-                fake = generator(fixed_noise)
+                fake = generator(kwargs["fixed_noise"])
                 save_image(fake.detach(),
                         '%s/fake_samples_epoch_%03d.png' % ("images", epoch),
                         normalize=True)
