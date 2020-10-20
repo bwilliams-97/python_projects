299e811679a6bde68f5c6d1b0735d726814683fb
Ben Williams <brw32@cam.ac.uk>
2020-10-20 11:40:03 +0100

    Added image cropping

--- cycle_gan/main.py
+++ cycle_gan/main.py
@@ -13,16 +13,44 @@ from tqdm import tqdm
 CYCLE_GAN_PATH = Path(os.path.abspath(__file__)).parent
 
 
-def ImageDataset(Dataset):
-    def __init__(self, images: List[np.ndarray]):
-        self._images = images
+class CGImageDataset(Dataset):
+    def __init__(self, images: List[np.ndarray], crop_images: bool=True, image_size: int = 512):
+        """
+        @param images: List of images represented as pixel values in numpy arrays.
+        @param crop_images: If True, images will be cropped to constant size.
+        @param image_size: Size of image to crop to.
+        """
+        # Image dimensions are [H x W x C]
+        if crop_images:
+            self._images = self.crop_images(images, image_size)
+        else:
+            self._images = images
+
+    def crop_images(self, images: List[np.ndarray], image_size: int) -> List[np.ndarray]:
+        """
+        Crop all images in dataset to same square size.
+        @param images: List of images to crop.
+        @param image_size: Side length of square (pixels).
+        """
+        processed_images = []
+        for image in images:
+            x_dim, y_dim = image.shape[0], image.shape[1]
+            # If image is too small, ignore
+            if np.min((x_dim, y_dim)) < image_size:
+                continue
+            # Otherwise select central portion
+            else:
+                start_x = x_dim // 2 - image_size // 2
+                start_y = y_dim // 2 - image_size // 2
+                processed_images.append(image[start_x : start_x + image_size, start_y : start_y + image_size])
+
+        return processed_images
 
     def __len__(self):
         return len(self._images)
 
     def __getitem__(self, idx):
         return torch.tensor(self._images[idx])
-
 
 
 def load_image_data(directory: Path) -> List[np.ndarray]:
@@ -32,7 +60,7 @@ def load_image_data(directory: Path) -> List[np.ndarray]:
     @returns List of numpy array representations of images.
     """
     image_files = find_files_with_extension(directory, "jpg")
-    image_files.extend(find_files_with_extension("png"))
+    image_files.extend(find_files_with_extension(directory, "png"))
 
     image_list = []
     
@@ -64,9 +92,10 @@ def main():
     lego_images = load_image_data(lego_image_dir)
     house_images = load_image_data(house_image_dir)
 
-    lego_dataset = ImageDataset(lego_images)
-    house_images = ImageDataset(house_images)
+    lego_dataset = CGImageDataset(lego_images)
+    house_images = CGImageDataset(house_images)
 
+    breakpoint()
 
 
 if __name__ == "__main__":
